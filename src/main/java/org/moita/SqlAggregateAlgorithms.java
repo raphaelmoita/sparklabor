1067dfd472f97e681e9b22d1085b994c2fd54bf7
Raphael Moita <raphael.moita@gmail.com>
2019-02-03 17:30:07 +0100

    include call that uses Java API version

--- src/main/java/org/moita/SqlAggregateAlgorithms.java
+++ src/main/java/org/moita/SqlAggregateAlgorithms.java
@@ -2,9 +2,11 @@ package org.moita;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.DataTypes;
+
+import static org.apache.spark.sql.functions.*;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -31,36 +33,72 @@ public class SqlAggregateAlgorithms {
 
         final Instant start = Instant.now();
 
-//        try (SparkSession spark = builder.getOrCreate()) {
-        SparkSession spark = builder.getOrCreate();
+        Dataset<Row> results;
 
-        data = spark.read()
-                .option("header", true)
-                .csv(filePath);
+        try (SparkSession spark = builder.getOrCreate()) {
 
-        data.createOrReplaceTempView("logging_table");
+            data = spark.read()
+                    .option("header", true)
+                    .csv(filePath);
 
-        // SortAggregate and HashAggregate (HashAggregation algorithms are much faster)
-        // All the fields that does NOT belong to GROUPING sections must be mutable in order
-        //  to Spark use HashAggregate algorithm.
+            data.createOrReplaceTempView("logging_table");
 
-        // Time elapsed: 45905 ms.
-        // SortAggregate: it has 'month_num' as String
-        Dataset<Row> results = spark.sql("SELECT level, DATE_FORMAT(datetime, 'MMMM') as month, FIRST(DATE_FORMAT(datetime, 'M')) as month_num, count(1) as total" +
-                " FROM logging_table" +
-                " GROUP BY level, month" +
-                " ORDER BY level, CAST(month_num as int)");
+            // SortAggregate and HashAggregate (HashAggregation algorithms are much faster)
+            // All the fields that does NOT belong to GROUPING sections must be mutable in order
+            //  to Spark use HashAggregate algorithm.
 
-        // Time elapsed: 15080 ms.
-        // HashAggregate: it has 'month_num' cast to int
-        results = spark.sql("SELECT level, DATE_FORMAT(datetime, 'MMMM') as month, FIRST(CAST(DATE_FORMAT(datetime, 'M') as int)) as month_num, count(1) as total" +
-                " FROM logging_table" +
-                " GROUP BY level, month" +
-                " ORDER BY level, month_num");
+            // Time elapsed: ~45s
+            // SortAggregate: it has 'month_num' as String
+//            results = sortedAggregateExecution(spark);
 
-        // Time elapsed: 15992 ms.
-        // HashAggregate: it has 'month_num' as part of grouping section
-        results = spark.sql("SELECT level, DATE_FORMAT(datetime, 'MMMM') as month, DATE_FORMAT(datetime, 'M') as month_num, count(1) as total" +
+            // Time elapsed: ~15s ms.
+            // HashAggregate: it has 'month_num' cast to int
+//            results = hashAggregatedExecution(spark);
+
+            // Time elapsed: ~15s ms.
+            // HashAggregate: it has 'month_num' as part of grouping section
+//            results = hashAggregatedExecution2(spark);
+
+            // Time elapsed: ~15s ms.
+            // Using Java API version
+            results = javaApiVersion();
+
+            results.show();
+
+            final Instant finish = Instant.now();
+
+            System.out.println("Time elapsed: " + Duration.between(start, finish).toMillis() + " ms.");
+
+            results.explain();
+        }
+    }
+
+    private Dataset<Row> javaApiVersion() {
+
+        Dataset<Row> results;
+        Column levelCol = col("level");
+        Column datetimeCol = col("datetime");
+
+        results = data.select(levelCol,
+                date_format(datetimeCol, "MMMM").alias("month"),
+                date_format(datetimeCol, "M").alias("month_num").cast(DataTypes.IntegerType));
+
+        RelationalGroupedDataset relationalGroupedDataset = results.groupBy("level", "month", "month_num");
+
+        results = relationalGroupedDataset.count().as("total");
+
+        results = results.orderBy("month_num");
+
+        // this column is being used ony for ordering by month. It is not possible to use 'month' column
+        // because we do NOT want to order it alphabetically.
+        results = results.drop("month_num");
+
+        return results;
+    }
+
+    private Dataset<Row> hashAggregatedExecution2(SparkSession spark) {
+
+        Dataset<Row> results = spark.sql("SELECT level, DATE_FORMAT(datetime, 'MMMM') as month, DATE_FORMAT(datetime, 'M') as month_num, count(1) as total" +
                 " FROM logging_table" +
                 " GROUP BY level, month, month_num" +
                 " ORDER BY level, month_num");
@@ -68,14 +106,34 @@ public class SqlAggregateAlgorithms {
         // this column is being used ony for ordering by month. It is not possible to use 'month' column
         // because we do NOT want to order it alphabetically.
         results.drop("month_num");
-        results.show();
 
-        final Instant finish = Instant.now();
+        return results;
+    }
 
-        System.out.println("Time elapsed: " + Duration.between(start, finish).toMillis() + " ms.");
+    private Dataset<Row> hashAggregatedExecution(SparkSession spark) {
 
-        results.explain();
-//    }
+        Dataset<Row> results = spark.sql("SELECT level, DATE_FORMAT(datetime, 'MMMM') as month, FIRST(CAST(DATE_FORMAT(datetime, 'M') as int)) as month_num, count(1) as total" +
+                " FROM logging_table" +
+                " GROUP BY level, month" +
+                " ORDER BY level, month_num");
+
+        // this column is being used ony for ordering by month. It is not possible to use 'month' column
+        // because we do NOT want to order it alphabetically.
+        results.drop("month_num");
+
+        return results;
+    }
+
+    private Dataset<Row> sortedAggregateExecution(SparkSession spark) {
+
+        Dataset<Row> results = spark.sql("SELECT level, DATE_FORMAT(datetime, 'MMMM') as month, FIRST(DATE_FORMAT(datetime, 'M')) as month_num, count(1) as total" +
+                " FROM logging_table" +
+                " GROUP BY level, month" +
+                " ORDER BY level, CAST(month_num as int)");
+
+        results.drop("month_num");
+
+        return results;
     }
 
     public static void main(String[] args) {
